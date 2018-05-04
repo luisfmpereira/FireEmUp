74dfe5d2f642ed1d3728e85cc832140f0f5846a7
Luis Filipe Modesto Pereira <luisf.modestop@gmail.com>
2018-05-04 02:55:24 -0300

    FirstSteps

---
+++ FireEmUp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public Transform playerTransf;
+	public Rigidbody2D playerRB;
+
+	public float moveSpeed = 5f;
+
+	private Vector3 mousePos;
+	private float angle;
+
+	public Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+		playerTransf = GetComponent<Transform> ();
+		playerRB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		movePlayer ();
+
+
+	}
+
+
+
+
+	void movePlayer(){
+
+		//move player according to axis inputs
+		playerTransf.position = new Vector2(playerTransf.position.x + Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime, playerTransf.position.y + Input.GetAxis ("Vertical")*moveSpeed*Time.deltaTime);
+
+
+		//rotate player according to mouse position
+
+		mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+		var dir = mousePos - playerTransf.position;
+		dir.Normalize ();
+		playerTransf.right = dir;
+
+		// LookAt 2D Unity
+
+	}
+}
