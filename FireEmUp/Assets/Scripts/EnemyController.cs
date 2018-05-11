ce7f5d2dfa45779dd568f6e53cd55432979e76e7
Luis Filipe Modesto Pereira <luisf.modestop@gmail.com>
2018-05-11 11:00:26 -0300

    10/05 changes

---
+++ FireEmUp/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	//move variables
+	public GameObject player;
+	private Transform enemyTransf;
+	public float moveSpeed;
+
+	//shooting variables
+	private float cooldown = 2f;
+	private float shootingCounter;
+	public Rigidbody2D bulletPrefab;
+	public bool allowShooting;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		enemyTransf = GetComponent<Transform> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		shootingCounter = 0f;
+	}
+	
+
+	void FixedUpdate () {
+		MoveEnemy ();
+
+		if(allowShooting)
+			ShootEnemy ();
+	}
+
+
+	void MoveEnemy(){
+		
+		enemyTransf.position = Vector2.MoveTowards (enemyTransf.position, player.transform.position, moveSpeed*Time.deltaTime);
+
+		//rotate enemy sprite towards player
+		var direction =  player.transform.position - enemyTransf.position;
+		var angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+
+		enemyTransf.rotation = Quaternion.Euler (0f, 0f, angle);
+	}
+
+	void ShootEnemy(){
+
+		shootingCounter += Time.deltaTime;
+
+		var direction =  (player.transform.position - enemyTransf.position);
+
+		if (shootingCounter >= cooldown) {
+			
+			shootingCounter = 0;
+
+			var bullet = Instantiate (bulletPrefab, enemyTransf.position, enemyTransf.rotation) as Rigidbody2D;
+
+			bullet.AddForce (300 * new Vector2(direction.x,direction.y).normalized);
+
+		}
+
+	}
+}
--- FireEmUp/Assets/Scripts/PlayerController.cs
+++ FireEmUp/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,20 @@ public class PlayerController : MonoBehaviour {
 
 	public Transform playerTransf;
 	public Rigidbody2D playerRB;
+	public Animator playerAnim;
 
 	public float moveSpeed = 5f;
-
+	public Camera cam;
 	private Vector3 mousePos;
 	private float angle;
 
-	public Camera cam;
 
 
 	// Use this for initialization
 	void Start () {
 		playerTransf = GetComponent<Transform> ();
 		playerRB = GetComponent<Rigidbody2D> ();
+		playerAnim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -31,14 +32,13 @@ public class PlayerController : MonoBehaviour {
 	}
 
 
-
-
 	void movePlayer(){
 
 		//move player according to axis inputs
 		playerTransf.position = new Vector2(playerTransf.position.x + Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime, playerTransf.position.y + Input.GetAxis ("Vertical")*moveSpeed*Time.deltaTime);
-
-
+		float speed = Mathf.Abs(Input.GetAxis ("Horizontal")) + Mathf.Abs(Input.GetAxis ("Vertical"));
+		playerAnim.SetFloat (moveSpeed,speed);
+			
 		//rotate player according to mouse position
 
 		mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -47,6 +47,24 @@ public class PlayerController : MonoBehaviour {
 
 		playerTransf.rotation = Quaternion.Euler(0f,0f,angle);
 
+
+	}
+
+
+	void OnCollisionEnter2D(Collision2D hit){
+		if (hit.gameObject.CompareTag ("Enemy")) {
+			//damage code
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+
+		if (hit.gameObject.CompareTag ("EnemyBullet")) {
+
+			Destroy (hit.gameObject,0);
+			//damage code
+		}
 
 	}
 }
